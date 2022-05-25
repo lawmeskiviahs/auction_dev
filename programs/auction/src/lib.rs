5e6b52fd04d9908068e0c625502caa2eebfe189c
lawmeskiviahs <shaiviksemwal@gmail.com>
2022-05-25 17:02:39 +0530

    struct datatype change

--- programs/auction/src/lib.rs
+++ programs/auction/src/lib.rs
@@ -5,7 +5,7 @@ use solana_program::{
     system_instruction,
 };
 
-declare_id!("ARwpwhLekSLvjZyphHWUVka2CRLc7aW4JHtXrifbnX1k");
+declare_id!("58h5LsVGRck3x8ksGK9BB9ke1zKrGMFgcqg12fRMGTe6");
 
 const AUCTION_SIGNER_SEEDS: &str = "testhuehuehuetest";
 
@@ -13,31 +13,33 @@ const AUCTION_SIGNER_SEEDS: &str = "testhuehuehuetest";
 pub mod auction {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialze>, price:u64, _bump:u8) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialze>, price:u32, _bump:u8) -> Result<()> {
 
         let auction_account: &mut Account<AuctionManager> = &mut ctx.accounts.auction_account;
         msg!("Pda account created");
 
-        auction_account.seller = *ctx.accounts.seller.key;
+       // auction_account.seller = ctx.accounts.seller.key.to_string();
+        auction_account.seller = "hello".to_string();
+
         auction_account.cost = price;
-        auction_account.mint = *ctx.accounts.mint.key;
+        auction_account.mint = ctx.accounts.mint.key.to_string();
         auction_account.is_on_sale = true;
 
         msg!("Pda account initialized");
         msg!("Transfer instruction made.");
         msg!("Transfer done");
-        msg!("Pda account.cost {}", auction_account.cost);
+        // msg!("Pda account.cost {}", auction_account.cost);
 
         Ok(())
     }
 
-    pub fn create_auction(ctx: Context<CreateAuction>, price:u64, _bump:u8) -> Result<()> {
+    pub fn create_auction(ctx: Context<CreateAuction>, price:u32, _bump:u8) -> Result<()> {
 
         let auction_account: &mut Account<AuctionManager> = &mut ctx.accounts.auction_account;
         
-        auction_account.seller = *ctx.accounts.seller.key;
+        auction_account.seller = ctx.accounts.seller.key.to_string();
         auction_account.cost = price;
-        auction_account.mint = *ctx.accounts.mint.key;
+        auction_account.mint = ctx.accounts.mint.key.to_string();
         auction_account.is_on_sale = true;
         
         msg!("Pda account fetch on create auction");
@@ -50,10 +52,10 @@ pub mod auction {
 
         let auction_account: &mut Account<AuctionManager> = &mut ctx.accounts.auction_account;
         
-        auction_account.buyer = ctx.accounts.buyer.key();
+        auction_account.buyer = ctx.accounts.buyer.key.to_string();
         
         msg!("Pda account fetch");
-        msg!("PDA {}", auction_account.cost);
+        // msg!("PDA {}", auction_account.cost);
 
         Ok(())
     }
@@ -114,7 +116,7 @@ pub struct CreateAuction<'info> {
     #[account(mut, signer)]
     /// CHECK XYZ
     seller:AccountInfo<'info>,
-    #[account(mut, constraint = mint.key() == auction_account.mint.key())]
+    // #[account(mut, constraint = mint.key() == auction_account.mint.key())]
     /// CHECK checked in program
     mint:AccountInfo<'info>,
     #[account(mut)]
@@ -136,7 +138,7 @@ pub struct BuyNFT<'info> {
     bump,
     )]
     auction_account: Account<'info, AuctionManager>,
-    #[account(mut, constraint = mint.key() == auction_account.mint.key())]
+    // #[account(mut, constraint = mint.key() == auction_account.mint.key())]
     /// CHECK checked in program
     mint:AccountInfo<'info>,
     #[account(mut)]
@@ -159,9 +161,9 @@ pub struct TransferSOL<'info> {
 #[account]
 #[derive(Default)]
 pub struct AuctionManager {
-    seller: Pubkey,
-    mint: Pubkey,
-    cost: u64,
-    buyer: Pubkey,
+    seller: String,
+    mint: String,
+    cost: u32,
+    buyer: String,
     is_on_sale: bool,
 }
